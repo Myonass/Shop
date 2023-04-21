ec53a439e0393cf765caf2e5af691a5dd4a5c331
Hittaky <tolmachev_maksim96@mail.ru>
2023-04-21 03:03:33 +0300

    added database

--- src/main/java/ru/tolmachev/buyandsell/controllers/ProductController.java
+++ src/main/java/ru/tolmachev/buyandsell/controllers/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.tolmachev.buyandsell.models.Product;
 import ru.tolmachev.buyandsell.services.ProductService;
 
@@ -16,26 +17,26 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/")
-    public String products(Model model){
-        model.addAttribute("products", productService.listProducts());
+    public String products(@RequestParam(name = "title", required = false) String title, Model model) {
+        model.addAttribute("products", productService.listProducts(title));
         return "products";
     }
-    @GetMapping("product/{id}")
-    public String productInfo(@PathVariable Long id, Model model){
-        model.addAttribute("product", productService.getProductById(id));
 
+    @GetMapping("/product/{id}")
+    public String productInfo(@PathVariable Long id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
         return "product-info";
     }
 
     @PostMapping("/product/create")
-    public String createProduct(Product product){
+    public String createProduct(Product product) {
         productService.saveProduct(product);
         return "redirect:/";
     }
 
     @PostMapping("/product/delete/{id}")
-    public String deleteProduct(@PathVariable Long id){
-        productService.deleteProducts(id);
+    public String deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
         return "redirect:/";
     }
 }
--- src/main/java/ru/tolmachev/buyandsell/services/ProductService.java
+++ src/main/java/ru/tolmachev/buyandsell/services/ProductService.java
@@ -1,36 +1,36 @@
 package ru.tolmachev.buyandsell.services;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.tolmachev.buyandsell.models.Product;
+import ru.tolmachev.buyandsell.repositories.ProductRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class ProductService {
+    private final ProductRepository productRepository;
 
-    private List<Product> products = new ArrayList<>();
-    private long ID = 0;
-    {
-        products.add(new Product(++ID,"Iphone 14 ", "Simple Description", 140000, "Simferopol", "Timur"));
-        products.add(new Product(++ID,"Intel Core i10 ", "Simple Description", 14000, "Krasnodar", "Timur"));
-    }
-    public List<Product> listProducts(){return products;}
-
-    public void saveProduct(Product product){
-        product.setId(++ID);
-        products.add(product);
+    public List<Product> listProducts(String title) {
+        if (title != null) return productRepository.findByTitle(title);
+        return productRepository.findAll();
     }
 
-    public void deleteProducts(Long id){
-        products.removeIf(product -> product.getId().equals(id));
+    public void saveProduct(Product product) {
+        log.info("Saving new {}", product);
+        productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        for (Product product : products) {
-            if(product.getId().equals(id)) return product;
-        }
-        return null;
+        return productRepository.findById(id).orElse(null);
     }
 }
